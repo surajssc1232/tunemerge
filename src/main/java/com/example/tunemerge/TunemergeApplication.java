6601bc6e43f816feb86b3fae9bca66766d3d7c7e
Suraj chauhan <surajssc1232@gmail.com>
2024-10-20 15:07:43 +0530

    init commit

---
+++ src/main/java/com/example/tunemerge/TunemergeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tunemerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunemergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunemergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tunemerge/TunemergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tunemerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunemergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
